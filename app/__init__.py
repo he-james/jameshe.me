c92dd1923744f363d7127121dc44038ce54eb1bc
James He <hejames98@gmail.com>
2019-11-25 16:28:25 -0500

    First successful run! Completed run.py, app/__init__.py, app/mod_main/models.py for now

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,24 @@
 """Create Flask object application."""
 
-from flask import Flask
-import main
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+# Configurations
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+# Error Handling
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+
+from app.mod_main.controllers import mod_main as main_module
+
+
+app.register_blueprint(main_module)
+
+
+db.create_all()
--- app/mod_main/controllers.py
+++ app/mod_main/controllers.py
@@ -1,5 +1,15 @@
-from app import app
+"""Render mod_main templates."""
 
-@app.route('/')
+from flask import Blueprint, request, render_template, redirect, url_for
+from app import db
+from app.mod_main.models import Project
+
+
+mod_main = Blueprint('main', __name__, url_prefix='/')
+
+
+@mod_main.route('/', methods=['GET', 'POST'])
 def home():
-    return
+    """Render home page."""
+    title = "Home | James He"
+    return render_template("main/home.html", title=title)
